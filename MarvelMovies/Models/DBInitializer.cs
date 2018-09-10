40a5a98fad54a9454dd2c5798839a2605964bbdc
porepullrequest <rogeryongluck@outlook.com>
2018-09-11 03:15:51 +0800

    database initializer

---
+++ MarvelMovies/Models/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace MarvelMovies.Models
+{
+    public class DBInitializer : DropCreateDatabaseIfModelChanges<MovieContext>
+    {
+        protected override void Seed(MovieContext context)
+        {
+            List<Movie> movies = MarvelMovies.Migrations.Configuration.GetMovies();
+            movies.ForEach(m => context.Movies.Add( m));
+        }
+    }
+}
