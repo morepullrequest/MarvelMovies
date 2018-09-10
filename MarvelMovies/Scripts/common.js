7e70c3edeceef8978da87fbfc445a386501880f5
porepullrequest <rogeryongluck@outlook.com>
2018-09-11 03:04:39 +0800

    update index

--- MarvelMovies/Scripts/common.js
+++ MarvelMovies/Scripts/common.js
@@ -5,9 +5,7 @@
         id = $(this).attr("id");
         movieID = id.split("-")[1];
         window.location.href = detailUrl + "?id=" + movieID;
-    }
+    });
 
-    );
     $(navItem).addClass("active");
-
 });
