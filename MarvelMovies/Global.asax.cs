d4d4cd57e47d0c64818d1a2728835260d1efbc80
porepullrequest <rogeryongluck@outlook.com>
2018-09-11 03:35:28 +0800

    initializer

--- MarvelMovies/Global.asax.cs
+++ MarvelMovies/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MarvelMovies.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,6 +20,9 @@ namespace MarvelMovies
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            Database.SetInitializer(new DBInitializer());
         }
     }
 }
